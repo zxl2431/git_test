a4c58a2982ecf24e02661526bb30228185cfbbd9
zhangxianlin <zhangxianlin2431@163.com>
2026-01-05 23:57:28 +0800

    main 分支提交一下

--- src/main/java/cn/mycc/App.java
+++ src/main/java/cn/mycc/App.java
@@ -8,7 +8,9 @@ public class App {
     System.out.println("hello git_4! hot-fix edit again");
     System.out.println("hello git_4! main-fix ");
 
-  System.out.println("hello git_5! edit in github! ");
+    System.out.println("hello git_5! edit in github! ");
+
+    System.out.println("----没有提交的东西是没有分支概念的---");
 
   }
 }
